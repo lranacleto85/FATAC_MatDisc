b8dbece195013f92def05a6011d213db4dff2cd2
Leonardo Ramos Anacleto <135238872+lranacleto85@users.noreply.github.com>
2023-09-29 14:12:03 -0300

    Update atividade2.c

--- atividade2.c
+++ atividade2.c
@@ -14,13 +14,13 @@
 // A funcao a ser implementada
 int fatorial(int n) 
 {
-    
     if (n == 0 || n == 1)
-        return -1;
-    else if (n<0) 
-        return -2. 
+        return 1;
+    else if (n < 0) 
+        return -2;
     else
         return n * fatorial(n - 1);
+}
 }
 
 
